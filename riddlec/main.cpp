48579b043f234503f5585445fd68abb8227ff059
zi2ven <wangziwen2011@icloud.com>
2025-07-17 19:36:36 +0800

    完全重写

--- riddlec/main.cpp
+++ riddlec/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 wangziwenhk
+ * Copyright (c) 2025 zi2ven
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,54 +15,6 @@
  *
  */
 
-#include <antlr4-runtime.h>
-#include <chrono>
-#include <filesystem>
-#include <iostream>
+int main(int argc, char *argv[]) {
 
-#include "args.h"
-#include "generate/config.h"
-#include "generate/Generate.h"
-#include "grammar/GramVisitor.h"
-#include "nodes/NodePrinter.h"
-#include "parser/RiddleLexer.h"
-#include "semantic/Analyzer.h"
-
-void init() {
-    riddle::globalContext = std::make_shared<llvm::LLVMContext>();
-}
-
-
-int main(int argc, char **argv) {
-    if (!riddle::parseArgs(argc, argv)) {
-        return -1;
-    }
-
-    std::ifstream in(riddle::build_args.inputFiles[0]);
-
-    std::stringstream buffer;
-    buffer << in.rdbuf();
-    std::string code = buffer.str();
-    antlr4::ANTLRInputStream input(code);
-    RiddleLexer lexer(&input);
-    antlr4::CommonTokenStream tokens(&lexer);
-    RiddleParser parser(&tokens);
-
-    auto tree = parser.program();
-    riddle::GramVisitor visitor;
-    auto result = visitor.nodeVisit(tree);
-
-    try {
-        riddle::Analyzer analyzer;
-        analyzer.visit(result);
-
-        init();
-        riddle::Generate generate;
-        generate.info->triple = llvm::Triple(riddle::build_args.triple);
-        generate.visit(result);
-        generate.info->buildToFile();
-    } catch (std::exception &e) {
-        std::cerr << e.what();
-        return 0;
-    }
 }
